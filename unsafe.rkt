23a42b87426f457c8abedf8d18c0f5da5c8ea981
Eric Griffis <eric@emdti.com>
2015-10-04 12:42:59 -0700

    add zmq_version output checks

--- unsafe.rkt
+++ unsafe.rkt
@@ -47,7 +47,11 @@
   (let ([major (box -1)]
         [minor (box -1)]
         [patch (box -1)])
-    (check equal? (zmq_version major minor patch) (void))))
+    (check equal? (zmq_version major minor patch) (void))
+    (check-true (> (unbox major) 0))
+    (check-true (> (unbox minor) 0))
+    (check-true (> (unbox patch) 0))
+    ))
 
 ;; ---------------------------------------------------------------------------
 ;; context
